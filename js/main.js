a989e6320c1edc6fbdb97160cf66226719016231
mireiasuefra <mireia.suefra@gmail.com>
2022-03-08 11:58:09 +0100

    add classlist red and green

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ console.log("hola");
 const questionElement = document.querySelector(".js-question");
 const button = document.querySelector(".button");
 
-let questions;
+let questions = [];
 let index = 0;
 
 function fetchQuestion() {
@@ -14,25 +14,55 @@ function fetchQuestion() {
     .then((data) => {
       questions = data;
       const element = data[index];
-      questionElement.innerHTML = renderHTML(element);
+      paintQuestion(element);
     });
 }
-
+ 
 function renderHTML(data) {
   let htmlCode = `<h2 class="question__title">${data.question}</h2>`;
   htmlCode += ` <img class="question__img" src="../images/${data.image}" alt="javascript">`;
-  htmlCode += `<p class="question__answer">${data.answer}</p>`;
-  htmlCode += `<p class="question__answer">${data.wrong_answer}</p>`;
-  htmlCode += `<p class="question__answer">${data.wrong_answer2}</p>`;
-
+  htmlCode += `<p class="question__answer js-answer">${data.answer}</p>`;
+  htmlCode += `<p class="question__answer js-answer">${data.wrong_answer}</p>`;
+  htmlCode += `<p class="question__answer js-answer">${data.wrong_answer2}</p>`;
   return htmlCode;
+}
+
+function addAnswerListeners() {
+  const answers =  document.querySelectorAll(".js-answer");
+  for (let index = 0; index < answers.length; index++) {
+    const element = answers[index];
+    element.addEventListener('click', handleClickAnswer);
+  }
+}
+
+function handleClickAnswer(event) {
+  event.preventDefault();
+  const answer = event.currentTarget.innerHTML;
+  const question = questions[index];
+
+  console.log('answer', answer);
+  console.log('question.answer', question.answer);
+
+  if (answer === question.answer) {
+    //console.log('ES CORRECTO')
+    event.currentTarget.classList.add('question__answer--right')
+  } else {
+    //console.log('ES INCORRECTO')
+    event.currentTarget.classList.toggle('question__answer--wrong')
+  }
+
+}
+
+function paintQuestion(element) {
+  questionElement.innerHTML = renderHTML(element);
+  addAnswerListeners();
 }
 
 function hanleClickNext(event) {
   event.preventDefault();
   if (index < questions.length - 1) {
     const element = questions[++index];
-    questionElement.innerHTML = renderHTML(element);
+    paintQuestion(element);
   }
 }
 
