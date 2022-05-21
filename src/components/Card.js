cd771f3dd55f0c2b1184d4bb46c91d1e07f8ef22
mireiasuefra <mireia.suefra@gmail.com>
2022-05-21 22:58:42 +0200

    add styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,16 @@
 function Card(props) {
-  console.log(props)
+  console.log(props);
 
   return (
-    <>
-      <h3>{props.question.question}</h3>
-      <img src={`images/${props.question.image}`} alt="imagen de la card"></img>
-      <p>{props.question.answer}</p>
-      <p>{props.question.wrongAnswer}</p>
-      <p>{props.question.wrongAnswer2}</p>
-    </>
+    <div className="card">
+      <h3 className="card__question">{props.question.question}</h3>
+      <img className="card__image" src={`images/${props.question.image}`} alt="imagen de la card"></img>
+      <div className="card__answers">
+      <p className="card__answer">{props.question.answer}</p>
+      <p className="card__answer">{props.question.wrongAnswer}</p>
+      <p className="card__answer">{props.question.wrongAnswer2}</p>
+      </div>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import logoHeader from "../images/Mireia.png";
-import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-      JAJAJAJA
+       <div>
+          <h1 className="header__title"> ¡Hola Mundo!</h1>
+          <h2 className="header__subtitle">
+            Soy Mireia y soy Frontend Developer.
+          </h2>
+        </div>
+        <img className="header__img" src={logoHeader} alt="Logo"></img>
     </header>
   );
 }
