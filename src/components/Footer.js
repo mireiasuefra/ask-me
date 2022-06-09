7cf8b14e7f40f83ee61291d4bccec8a8ac32231d
mireiasuefra <mireia.suefra@gmail.com>
2022-06-09 15:04:38 +0200

    add link email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,14 +14,26 @@ function Footer() {
           <a
             className="footer__rrss-web"
             href="https://github.com/mireiasuefra"
+            target="_blank"
+            rel="noreferrer"
           >
             <i className="icon fab fa-github-square"></i>
           </a>
           <a
             className="footer__rrss-web"
             href="https://www.linkedin.com/in/mireia-suefra/"
+            target="_blank"
+            rel="noreferrer"
           >
             <i className="icon fab fa-linkedin"></i>
+          </a>
+          <a
+            className="footer__rrss-web"
+            href="mailto:mireia.suefra@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="icon fa fa-envelope"></i>
           </a>
         </div>
       </section>
